e3d95d88c68cd776f42a5da4729e8b0c53adbdc3
Pin Cheng <orangebaba@icloud.com>
2019-05-20 15:45:24 +0800

    Adjust code intend

--- c-cpp/constmemfunc.cpp
+++ c-cpp/constmemfunc.cpp
@@ -6,31 +6,31 @@ using std::endl;
 /*
  * 常量成员函数可以被非常量对象调用
  * 常量对象只能调用常量成员函数
+ * 常量引用的参数对象也当作常量对象进行处理
  */
 class B {
 private:
-    int _c;
+  int _c;
+
 public:
-    B():_c(1) {
+  B() : _c(1) {}
 
-    }
+  int calc() const { return _c; }
 
-    int calc() const {
-        return _c;
-    }
-
-    int getc() {
-        return _c;
-    }
+  int getc() { return _c; }
 };
 
-int main(int argc, const char* argv[]) {
-    const B b;
-    b.calc();
-    // b.getc();
-    B b1;
+void foo(const B &c) {
+  // c.getc();
+}
 
-    b1.calc();
+int main(int argc, const char *argv[]) {
+  const B b;
+  b.calc();
+  // b.getc();
+  B b1;
 
-    return 0;
+  b1.calc();
+
+  return 0;
 }
